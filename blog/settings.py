49022d37b3064403bff92ff3ab63806ca3ac445c
Jing <yzzok0223@gmail.com>
2019-10-20 22:10:34 +0900

    commit-29

--- blog/settings.py
+++ blog/settings.py
@@ -26,81 +26,6 @@ SECRET_KEY = '*u(elr9%xm1_4!m$at8#a6f3un2mwlkc5(sdbju8gmr(ln*r%v'
 DEBUG = True
 
 
-'''
-    django：将 django 产生的所有消息转交给 console 处理器
-    django.request：将网络请求相关消息转交给 file、mail_admins 这两个处理器。
-    注意这里的 'propagate': False 使得此记录器处理过的消息就不再让 django 记录器再次处理了
-    自定义 log
-    from my_blog.settings import LOGGING
-    import logging
-    
-    logging.config.dictConfig(LOGGING)
-    logger = logging.getLogger('django.request')
-    
-    def whatever(request):
-        # do something
-        logger.warning('Something went wrong!')
-        # do something else
-'''
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-            'verbose': {
-                # 详细的格式化器，依次输出：消息级别、发生时间、抛出模块、进程ID、线程ID、提示信息
-                'format': '{asctime} {levelname} {module} {process:d} {thread:d} {message}',
-                'style': '{',
-            },
-            # 简要的格式化器，输出时间，消息级别和提示信息
-            'simple': {
-                'format': '{asctime} {levelname} {message}',
-                'style': '{',
-            },
-    },
-    'filters': {
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'handlers': {
-        # INFO 以上级别消息，输出简要信息到命令行中；此处理器仅在调试模式生效
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'], # 使用此过滤器的消息仅在调试时才会生效
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        # ERROR 以上级别消息，输出详细信息到 Email 中
-        # 'mail_admins': {
-        #     'level': 'ERROR',
-        #     'class': 'django.utils.log.AdminEmailHandler',
-        #     'formatter': 'verbose',
-        # },
-        # WARNING 以上级别消息，输出详细信息到文件中
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.TimedRotatingFileHandler',   # Python 内置的随时间分割日志文件的模块
-            'when': 'midnight',                                     # 分割时间为凌晨
-            'backupCount': 30,                                      # 日志文件保存日期为30天
-            'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console','file'],
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    }
-}
-
-
 ALLOWED_HOSTS = []
 
 
@@ -112,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',  # allauth 启动必须项
     'django.contrib.staticfiles',
     'password_reset',
     'article',
@@ -127,6 +53,13 @@ INSTALLED_APPS = [
     # 富文本编辑器
     'ckeditor',
     'ckeditor_uploader',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # 可添加需要的第三方登录
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.weixin',
 ]
 
 MIDDLEWARE = [
@@ -150,7 +83,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                'django.contrib.auth.context_processors.auth',  # allauth 启动必须项
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -213,13 +146,17 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+
+
 # 媒体文件保存地址
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CKEDITOR_UPLOAD_PATH = 'upload/'
 CKEDITOR_IMAGE_BACKEND = 'pillow'
 
+
 # 富文本编辑器 Config
+
 CKEDITOR_CONFIGS = {
     # django-ckeditor 默认使用 default 配置
     'default':{
@@ -240,15 +177,112 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-# SMTP服务器，改为你的邮箱的smtp!
+# 日志文件的配置
+
+'''
+    django：将 django 产生的所有消息转交给 console 处理器
+    django.request：将网络请求相关消息转交给 file、mail_admins 这两个处理器。
+    注意这里的 'propagate': False 使得此记录器处理过的消息就不再让 django 记录器再次处理了
+    自定义 log
+    from my_blog.settings import LOGGING
+    import logging
+
+    logging.config.dictConfig(LOGGING)
+    logger = logging.getLogger('django.request')
+
+    def whatever(request):
+        # do something
+        logger.warning('Something went wrong!')
+        # do something else
+'''
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            # 详细的格式化器，依次输出：消息级别、发生时间、抛出模块、进程ID、线程ID、提示信息
+            'format': '{asctime} {levelname} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        # 简要的格式化器，输出时间，消息级别和提示信息
+        'simple': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        # INFO 以上级别消息，输出简要信息到命令行中；此处理器仅在调试模式生效
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],  # 使用此过滤器的消息仅在调试时才会生效
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        # ERROR 以上级别消息，输出详细信息到 Email 中
+        # 'mail_admins': {
+        #     'level': 'ERROR',
+        #     'class': 'django.utils.log.AdminEmailHandler',
+        #     'formatter': 'verbose',
+        # },
+        # WARNING 以上级别消息，输出详细信息到文件中
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',  # Python 内置的随时间分割日志文件的模块
+            'when': 'midnight',  # 分割时间为凌晨
+            'backupCount': 30,  # 日志文件保存日期为30天
+            'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
+
+
+# 第三方登录 allauth 配置
+
+AUTHENTICATION_BACKENDS = (
+    # Django 后台可独立于 allauth 登录
+    'django.contrib.auth.backends.ModelBackend',
+
+    # 配置 allauth 独有的认证方法，如 email 登录
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+# 设置站点
+SITE_ID = 1
+
+# 登录成功后重定向地址
+LOGIN_REDIRECT_URL = '/'
+
+
+
+# 邮箱配置
+
+# SMTP服务器
 EMAIL_HOST = 'smtp.gmail.com'
-# 改为你自己的邮箱名！
-EMAIL_HOST_USER = 'your_email_account@xxx.com'
-# 你的邮箱密码
-EMAIL_HOST_PASSWORD = 'your_password'
+# 邮箱名！
+EMAIL_HOST_USER = 'yzok0223@gmail.com'
+# 邮箱密码
+EMAIL_HOST_PASSWORD = '693477ok'
 # 发送邮件的端口
-EMAIL_PORT = 25
+EMAIL_PORT = 587
 # 是否使用 TLS
 EMAIL_USE_TLS = True
 # 默认的发件人
-DEFAULT_FROM_EMAIL = 'your_email_account@xxx.com'
+DEFAULT_FROM_EMAIL = 'yzok0223@gmail.com'
--- blog/urls.py
+++ blog/urls.py
@@ -18,8 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from article.views import ArticleListView
 
 urlpatterns = [
+    # home
+    path('', ArticleListView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     # 直接返回视图，不渲染
     path('404/',TemplateView.as_view(template_name='404.html'), name='page_404'),
@@ -28,6 +31,7 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('password-reset/', include('password_reset.urls')),
     path('comment/', include('comment.urls', namespace='comment')),
+    path('accounts/', include('allauth.urls'))
 ]
 
 # settings.py 中添加了 MEDIA_URL MEDIA_ROOT，在这里配置媒体文件的 URL
